f01f1fccdb87808daa592720795d15f3e3fca128
gunger28 <gungeranton@gmail.com>
2021-08-16 03:28:07 +0700

    from pc

--- js/index.js
+++ js/index.js
@@ -9,12 +9,12 @@ function darwAll(){
     let sizeV = 27
     let counterV = 0
     for (let i = 1; i < size+1; i++) {
-    body.innerHTML += "<img src=\"../assets/img/" + i + "i.png\">"
+    body.innerHTML += "<img src=\"./assets/img/" + i + "i.png\">"
         const decider = Math.random() * 100
         if(decider < 50 && counterV < sizeV){
             counterV = counterV + 1
              body.innerHTML += "<video autoplay muted loop controls=\"controls\">" +
-             "<source src=\"../assets/vid/v (" + counterV + ").webm\"" + 
+             "<source src=\"./assets/vid/v (" + counterV + ").webm\"" + 
              "</video>"
             // body.innerHTML += "<div>" + counterV + "</div>"
         }
